7997a84fc074a96c1dbbcd63a500fa0f6f605c98
AcidSepp <spritzkopflp@gmail.com>
2025-06-05 16:21:10 +0200

    modernize

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+sudo cp isoimage/root/etc/systemd/system/samplerbox.service /media/yannick/rootfs/etc/systemd/system/samplerbox.service
+sudo cp samplerbox.py /media/yannick/rootfs/root/SamplerBox/samplerbox.py
+sudo cp isoimage/root/root/SamplerBox/config.py /media/yannick/rootfs/root/SamplerBox/config.py
